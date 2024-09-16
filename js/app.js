ca03f1a3035d23b7719e4bc4d98f4af80db6e60d
jorge <jorgelizarraga1994@gmail.com>
2024-09-15 21:44:44 -0300

    commit 15-09 at 21:44

--- js/app.js
+++ js/app.js
@@ -4,14 +4,18 @@ inputDescripcion = document.getElementById('descriptionInput')
 divContenedor = document.getElementById('containerCards')
 
 boton.addEventListener("click", ()=>{
-    if(inputTarea.value != ''){
+    if(inputTarea.value != '' && inputDescripcion.value != ''){
         divContenedor.innerHTML +=`<div class="card" style="width: 18rem;">
-                                <div class="card-body">
-                                    <h5 class="card-title">${inputTarea.value}</h5>
-                                    <p class="card-text">${inputDescripcion.value}</p>
-                                </div>
-                                </div>`
+                                        <div class="card-body">
+                                            <h5 class="card-title">${inputTarea.value}</h5>
+                                            <p class="card-text">${inputDescripcion.value}</p>
+                                        </div>
+                                        <button class="btn btn-danger">delete</button>
+                                    </div>`
         inputTarea.value = ''
         inputDescripcion.value = ''
+    }
+    else{
+        alert('Campos vacios, ingrese datos para crear un recordatorio')
     }                          
 })
